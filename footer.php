f3c64271d87d0feecb309a69f1b8ff46c4f7ed65
Yahya325-lab <stellarnraven@gmail.com>
2024-12-03 12:48:02 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+    <div class="container">
+        <footer>
+            <p>&copy; 2021, Informatika, Universitas Pelita Bangsa</p>
+        </footer>
+    </div>
+</body>
+</html>
